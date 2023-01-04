db5d9c4df45d6d18e30fc9b33ce24f19b6dc1dd7
alandevper <alandevper@gmail.com>
2023-01-04 23:48:57 +0900

    Update

---
+++ src/components/room.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="black-bg">
+    <div class="white-bg">
+      <img class="room-img" :src="room.image" />
+      <h4>{{room.title}}</h4>
+      <p>{{room.content}}</p>
+      <p>{{room.price}} 원</p>
+      <button @click="$emit('closeRoomModal')">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'RoomModal',
+    props: {
+        room : Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
